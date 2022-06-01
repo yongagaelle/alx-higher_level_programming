b40d2951aa3c8e51ddc02b543d6d4135c55bb7aa
yongagaelle <yongagaellemanuela@gmail.com>
2022-06-01 03:30:49 -0700

    update 0x01

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,10 @@ if number < 0:
     last_digit = -(-number % 10)
 else:
     last_digit = number % 10
+msg1 = "Last digit of"
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(msg1, f"{number} is {last_digit} and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
     if char % 2 == 1:
-            char = char - 32
+        char = char - 32
     print("{:c}".format(char), end="")
