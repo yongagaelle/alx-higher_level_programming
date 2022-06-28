cd0bbeef6e4418b8a90b90bb0c974b597fe81298
yongagaelle <yongagaellemanuela@gmail.com>
2022-06-29 00:07:35 +0200

    0x09-python

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    setattr(magic_string, "n", getattr(magic_string, "n", 0) + 1)
-    return ("BestSchool", " * getattr(magic_string, "n", 0))[:-2]
+def magic_string(H=[]):
+    H += ["BestSchool"]
+    return (", ".join(H))
