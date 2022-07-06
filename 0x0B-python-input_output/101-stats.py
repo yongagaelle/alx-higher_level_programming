898268b67e35227de0a65f287268d3bf542d756a
yongagaelle <yongagaellemanuela@gmail.com>
2022-07-06 08:00:33 +0200

    update project python input output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,55 +1,38 @@
-#!/usr/bin/python3ads from standard input and computes metrics.
+#!/usr/bin/python3
+'''
+Script for log parsing
+'''
+import sys
 
-"""
-Write a script that reads stdin line by line and computes metrics:
-"""
-
-
-def print_stats(size, status_codes):
-    """Print accumulated metrics.
-
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
 
 if __name__ == "__main__":
-    import sys
-
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
-
+    a = []
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    dictio = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+              "405": 0, "500": 0}
+    i = 0
+    file_size = 0
     try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
+        for lines in sys.stdin:
+            a = lines.split(" ")
+            if "\n" in a[-1]:
+                value = a[-1][:-1]
             else:
-                count += 1
-
-            line = line.split()
-
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+                value = a[-1]
+            if len(a) > 2 and a[-2] in codes and value.isnumeric() is True:
+                dictio[a[-2]] += 1
+                file_size += int(value)
+                i += 1
+            if i % 10 == 0:
+                print("File size: {}".format(file_size))
+                for code in codes:
+                    if dictio[code] > 0:
+                        print("{}: {}".format(code, dictio[code]))
+        exit()
+    except Exception:
+        pass
+    finally:
+        print("File size: {}".format(file_size))
+        for code in codes:
+            if dictio[code] > 0:
+                print("{}: {}".format(code, dictio[code]))
